c3ad2873f8564a0bdfc334ec273bbde6bff35597
igordmouraa <igomora04@gmail.com>
2024-08-13 13:27:27 -0300

    feat: Add response time calculation to health check route

--- src/index.js
+++ src/index.js
@@ -5,24 +5,35 @@ dotenv.config();
 const configDatabase = require('./database/index');
 
 class AppController {
-    constructor()
-    {
+    constructor() {
         this.express = express();
         this.middlewares();
         this.router();
     }
-    middlewares(){
+
+    middlewares() {
         this.express.use(express.json());
+        this.express.use((req, res, next) => {
+            req.startTime = Date.now();
+            next();
+        });
     }
 
-    router()
-    {
+    router() {
         this.express.use('/petshopApi', require('./appModule'));
-    }
+        this.express.get("/health/", (req, res) => {
+            // Calcula o tempo decorrido
+            const duration = Date.now() - req.startTime;
 
+            res.send({
+                status: "Application Up",
+                message: `HTTP request received. Response successfully rendered in ${duration}ms`
+            });
+        });
+    }
 }
 
-function createApiController(){
+function createApiController() {
     return new AppController();
 }
 
