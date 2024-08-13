82e024d44ad16a481fdf3738850f54538de5d36a
igordmouraa <igomora04@gmail.com>
2024-08-13 15:51:39 -0300

    feat: Added user module

--- src/appModule.js
+++ src/appModule.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const userModule =  require('./user/user.module');
+
+router.use('/user', userModule);
+
 module.exports = router;
