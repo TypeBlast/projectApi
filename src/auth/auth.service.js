52400de1cc09af12c197b66f5c7c4004c44a1bff
vinicius souza <vinicius.souza.oliveira123@gmail.com>
2024-08-28 21:13:13 -0300

    chore: home page development

--- src/auth/auth.service.js
+++ src/auth/auth.service.js
@@ -5,27 +5,29 @@ const login = async (email, password) => {
     try {
         const user = await User.findOne({ where: { email } });
 
-
         if (!user || !(await user.verifyPassword(password))) {
             throw new Error('Credenciais inválidas');
         }
 
         const token = generateToken(user);
 
-    
         return {
             status: 201,
             message: 'Login bem-sucedido',
-            token
+            token,
+            user: {
+                name: user.name, // Retorne o nome do usuário
+                email: user.email,
+            },
         };
 
     } catch (e) {
-    
         return {
             status: 400,
             message: e.message,
         };
     }
 };
+
 
 module.exports = { login };
