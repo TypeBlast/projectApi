d40193eaf9c35efe627d017d2060ca1a50a367d1
igordmouraa <igordmoura_@hotmail.com>
2024-09-03 15:19:57 -0300

    config(database): update database configuration with environment variables and timezone

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,24 @@
 require('dotenv').config();
 
 module.exports = {
-    dialect: 'mysql', 
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE,
-    define: {
-        timestamps: false,
-        underscored: true,
-        underscoredAll: true,
-        createdAt: 'created_at',
-        updatedAt: 'updated_at'
-    },
-    dialectOptions: {
-        timezone: 'local', 
-    },
-    timezone: 'America/Sao_Paulo' 
+    development: {
+        dialect: 'mysql',
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE,
+        define: {
+            timestamps: false,
+            underscored: true,
+            underscoredAll: true,
+            createdAt: 'created_at',
+            updatedAt: 'updated_at'
+        },
+        dialectOptions: {
+            timezone: 'local',
+        },
+        timezone: 'America/Sao_Paulo'
+    }   
+
 };
--- src/database/index.js
+++ src/database/index.js
@@ -1,20 +1,21 @@
 const { Sequelize } = require('sequelize');
 const databaseConfig = require('../config/database');
 
+// Seleciona a configuração com base no ambiente
+const env = process.env.NODE_ENV || 'development';
+const config = databaseConfig[env];
+
+const sequelize = new Sequelize(config);
+
 const User = require('../user/Entities/user.entity');
 const States = require('../state/Entities/states.entity');
-const Cities = require('../city/Entities/cities.entity'); 
-const Addresses = require('../address/Entities/addresses.entity')
-
-
-const sequelize = new Sequelize(databaseConfig);
-
+const Cities = require('../city/Entities/cities.entity');
+const Addresses = require('../address/Entities/addresses.entity');
 
 User.init(sequelize);
 States.init(sequelize);
 Cities.init(sequelize);
-Addresses.init(sequelize)
-
+Addresses.init(sequelize);
 
 States.hasMany(Cities, {
   foreignKey: 'state_id',
@@ -27,30 +28,28 @@ Cities.belongsTo(States, {
 });
 
 Cities.hasMany(Addresses, {
-    foreignKey: 'city_id',
-    as: 'addresses'
-  });
-  
-Addresses.belongsTo(Cities, {
-    foreignKey: 'city_id',
-    as: 'cities'
-  });
-  
-User.hasMany(Addresses, {
-    foreignKey: 'user_id',
-    as: 'addresses'
+  foreignKey: 'city_id',
+  as: 'addresses'
 });
-  
-Addresses.belongsTo(User, {
-    foreignKey: 'user_id',
-    as: 'user'
-  });
 
+Addresses.belongsTo(Cities, {
+  foreignKey: 'city_id',
+  as: 'cities'
+});
+
+User.hasMany(Addresses, {
+  foreignKey: 'user_id',
+  as: 'addresses'
+});
+
+Addresses.belongsTo(User, {
+  foreignKey: 'user_id',
+  as: 'user'
+});
 
 sequelize.sync()
   .then(() => console.log('Banco de dados sincronizado.'))
   .catch(error => console.error('Erro ao sincronizar o banco de dados:', error));
-
 
 module.exports = {
   User,
