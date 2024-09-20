74477edff1ab708a6a420edc9bba013894fd904a
VictorSantuccii <victorfregne@gmail.com>
2024-09-20 19:57:24 -0300

    fix(employers): adding validations within employee creation

--- src/employers/employers.service.js
+++ src/employers/employers.service.js
@@ -1,25 +1,25 @@
 const Employer = require('./Entities/employers.entity');
 
 class EmployerService {
+
   async createEmployer(employerData) {
     try {
 
-      if (!employerData.name || !employerData.phone || !employerData.position) {
-        throw new Error('Campos obrigatórios ausentes. Os campos nome, telefone e cargo são necessários.');
+      if (!employerData.name || !employerData.phone || !employerData.position || !employerData.service_id) {
+        throw new Error('Campos obrigatórios ausentes. Os campos nome, telefone, cargo e ID do serviço são necessários.');
       }
 
-   
+
       if (!/^\d{13}$/.test(employerData.phone)) {
         throw new Error('O telefone deve conter 13 dígitos numéricos, incluindo o código postal e seu DDD.');
       }
 
-    
       const existingEmployerByPhone = await Employer.findOne({ where: { phone: employerData.phone } });
       if (existingEmployerByPhone) {
         throw new Error('Telefone já registrado.');
       }
 
-  
+   
       const newEmployer = await Employer.create(employerData);
       return { status: 201, message: 'Funcionário criado com sucesso.' };
     } catch (e) {
@@ -76,24 +76,30 @@ class EmployerService {
     }
   }
   
-  async updateEmployer(employerId, employerData) {
+ async updateEmployer(employerId, employerData) {
     try {
    
       if (!employerId || isNaN(employerId)) {
         throw new Error('Id do funcionário inválido.');
       }
 
-     
+      
       if (employerData.phone && !/^\d{13}$/.test(employerData.phone)) {
         throw new Error('O telefone deve conter 13 dígitos numéricos.');
       }
+
 
       const employer = await Employer.findByPk(employerId);
       if (!employer) {
         throw new Error('Funcionário não encontrado.');
       }
 
-    
+
+      if (employerData.service_id && isNaN(employerData.service_id)) {
+        throw new Error('ID do serviço inválido.');
+      }
+
+
       await employer.update(employerData);
       return { status: 200, message: 'Dados do funcionário atualizados com sucesso' };
     } catch (e) {
