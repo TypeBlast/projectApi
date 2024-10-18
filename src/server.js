3ecb4c1ac1f7c5dc2b496a79a816f335325cd07b
igordmouraa <igordmoura_@hotmail.com>
2024-10-18 09:51:46 -0300

    chore(cors): configure cors for specific domains

--- src/server.js
+++ src/server.js
@@ -1,6 +1,19 @@
 require('dotenv').config();
+const cors = require('cors');  // Importando o cors
 const app = require('./index');
 const PORT = process.env.PORT;
+
+// Configuração do CORS
+const corsOptions = {
+    origin: [
+        'https://petexpress.vercel.app',
+        'https://petexpress-typeblast.vercel.app'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.listen(PORT, () => {
     console.log(`Servidor conectado e rodando na porta ${PORT} 💾`);
